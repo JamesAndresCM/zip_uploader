6042fa3b6a5cd62c0a9a41c11b6389861d5409be
JamesACM <jaimecanalesmarin@gmail.com>
2018-09-09 11:46:25 -0300

    use callback to remove file

--- app/models/file_resource.rb
+++ app/models/file_resource.rb
@@ -1,4 +1,6 @@
 class FileResource < ApplicationRecord
+  include UploadFile
+  before_destroy -> { delete_file(name) }
   has_one :pet, dependent: :destroy
   has_many :products, dependent: :destroy, inverse_of: :file_resource
   validates_presence_of :name
